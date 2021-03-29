2950e197595e2107b5f95f6bb921fa18e129225a
tapassaha699 <tapassaha699@gmail.com>
2021-03-30 03:05:00 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// API - https://forkify-api.herokuapp.com/
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<Your-key>';
+export const MODAL_CLOSE_SEC = 2.5;
